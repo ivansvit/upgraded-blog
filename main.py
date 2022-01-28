0d7b51bf54a6bc5a801c6e53ba78164f43f5cf8b
IvanSvit <e#456yhn>
2022-01-28 21:51:33 +0100

    Install PostgreSQL

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 Bootstrap(app)
 ckeditor = CKEditor(app)
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
